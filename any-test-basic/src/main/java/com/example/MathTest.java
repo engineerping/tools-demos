b164ed6b0aa63038a510ff7b088f3823b6e3991f
engineerping <gongchengship@163.com>
2022-05-27 01:24:56 +0800

    统一Spring boot版本

--- any-test-basic/src/main/java/com/example/MathTest.java
+++ any-test-basic/src/main/java/com/example/MathTest.java
@@ -3,14 +3,31 @@ package com.example;
 /**
  * <p>TIPS: Character set of current file is "UTF-8",just in case of Chinese characters displays in garbled</p>
  * 描述:
+ *
+ * java的除法,和取模运算的特殊规则
+ * 整数进行除法,取模运算,时得到的结果仍然是整数
+ * 想要得到小数,
+ * 就要让被除数或者除数为小数(float或double)
+ *
+ * 另外用flaot 或者double 去存两个整数的(除法或取模)运算结果,也能够得到十分位为0的分小数
  */
 public class MathTest {
     public static void main(String[] args) {
         int i = 1537351;
         int j = i / 1000;
         float k = i / 1000;
+        int j1 = 3 / 5;
+        float k1 = 3 / 5;
+        int j2 = 5 / 3;
+        float k2 = 5 / 3;
+        double k3 = 1000 / 3.0;
         System.out.println(j);
         System.out.println(k);
+        System.out.println(j1);
+        System.out.println(k1);
+        System.out.println(j2);
+        System.out.println(k2);
+        System.out.println(k3);
         System.out.println("-----");
 
         int l = i % 10;
--- basic-class-loader/src/main/java/com/example/class_loader_flow/snapshot.md
+++
@@ -1,5 +0,0 @@
-##
-![img.png](ClassLoadingFlow_1.png)
-![img.png](ClassLoadingFlow_2.png)
-![img.png](ClassLoadingFlow_3.png)
-![img.png](ClassLoadingFlow_4.png)
