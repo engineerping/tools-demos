f257bd8cdffb4263008149cfdf8bb9304ace62fc
engineerping <gongchengship@163.com>
2023-08-09 11:14:02 +0800

    添加 Design pattern 模块

--- any-test-spring/src/main/java/com/example/anytest/postconstructanotaion/HelloController.java
+++ any-test-spring/src/main/java/com/example/anytest/postconstructanotaion/HelloController.java
@@ -1,18 +1,17 @@
 package com.example.anytest.postconstructanotaion;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>TIPS: Character set of current file is "UTF-8",just in case of Chinese characters displays in garbled</p>
  * 描述:
  */
-@Controller
+@RestController
 public class HelloController {
 
     /**
-     * 只需要把这个bean 放到servlet 容器中，就可以出发@Constructor注解的方法执行,还可触发多个
+     * 只需要把这个bean 放到servlet 容器中，就可以触发@Constructor注解的方法执行,还可触发多个
      */
     @Autowired
     private PostConstructAnotation postConstructAnotation;
