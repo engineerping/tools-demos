a3271e8a372524454c7e380a5b7a687312805f4c
engineerping <gongchengship@163.com>
2023-10-13 14:07:33 +0800

    实验二完成
    # Conflicts:
    #	spring-all-design-pattern/_Notes/2.结构型模式/0.细分区别.md

--- basic-self-learning-exam/src/main/java/com/example/practice3/I_JudgePrime.java
+++ basic-self-learning-exam/src/main/java/com/example/practice3/I_JudgePrime.java
@@ -1,6 +1,30 @@
 package com.example.practice3;
-
+/**
+ * 判断素数
+ * 总结: 素数的概念很重要,
+ */
 public class I_JudgePrime {
+    /**
+     * 如果N不能被2 ~ sqrt(N) 闭区间之间任一整数整除，那么N必定是素数
+     * @param num
+     * @return
+     */
+    public static boolean isPrime(int num) {
+        boolean flag = false;
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) { //如果N不能被2 ~ sqrt(N) 之间任一整数整除，那么N必定是素数
+                return false;
+            }
+        }
+        return true;
+    }
+    /**
+     * main方法, 从命令行接收参数
+     * @param args
+     */
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         if (isPrime(n)) {
@@ -8,26 +32,5 @@ public class I_JudgePrime {
         } else {
             System.out.println(n + " is not prime number");
         }
-    }
-
-    public static boolean isPrime(int num) {
-        boolean flag = false;
-        if (num <= 1) {
-            return false;
-        }
-
-        for (int i = 2; i < num; i++) {
-            if (num % i == 0) { //能被除了 1 和 自身之外的任何整数整除的数,就不是素数
-                return false;
-            }
-        }
-
-        ////方法二
-        for (int i = 2; i < Math.sqrt(num); i++) {
-            if (num % i == 0) { //如果N不能被2 ~ sqrt(N) 之间任一整数整除，那么N必定是素数
-                return false;
-            }
-        }
-        return true;
     }
 }
