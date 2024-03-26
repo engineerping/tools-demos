eee280e29593b18c295b9f032b21cd3df5e06500
engineerping <gongchengship@163.com>
2024-03-26 16:33:31 +0800

    添加模块 basic-interview-questions 和 spring-interview-questions

---
+++ spring-bean-life-circle/src/main/java/com/example/anytest/SpringBeanLifeCircleApplication.java
@@ -0,0 +1,13 @@
+package com.example.anytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeanLifeCircleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBeanLifeCircleApplication.class, args);
+    }
+
+}
