3459b4657c4f80fbf28dce26685a6391b5cea1cc
engineerping <gongchengship@163.com>
2024-03-31 17:41:17 +0800

    RocketMQ

---
+++ _Notes/Spring面试题[图灵课堂周瑜].md
@@ -0,0 +1,58 @@
+1. Spring是如何创建一个Bean对象的？
+2. 【Spring面试核心技术】Spring源码
+3. 【2022面试热点】微服务Spring Cloud Alibaba三高架构实战
+4. 什么是单例池？作用是什么？
+5. Bean对象和普通对象之间的区别是什么？
+6. @PostConstruct注解是如何工作的？
+7. Bean的初始化是如何工作的？
+8. Bean的初始化和实例化区别是什么？
+9. 什么是初始化后？
+10. 推断构造方法是什么意思？
+11. 什么是先bytype再byName
+12. Spring AOP底层是怎么工作的？
+13. Spring事务底层是怎么工作的？
+14. @Configuration注解的作用是什么？
+15. Spring为什么要用三级缓存来解决循环依赖？
+16. @Async为什么会导致循环依赖解决不了？
+17. 构造方法和多例导致的循环依赖
+18. SpringMVC处理请求底层原理流程
+19. SpringMVC容器创建源码分析
+20. SpringMVC父子容器原理分析
+21. SpringMVC父子容器源码解析
+22. SpringMVC的零配置
+23. 手写代码模拟SpringBoot
+24. Spring Boot如何选择Tomcat还是Jett底层
+25. @ConditionalOnClass注解底层原理
+26. Spring Boot零配置底层原理实现
+27. spring是什么
+28. spring的事务传播机制
+29. Spring框架中Bean的创建过程2
+30. Spring框架中的Bean是线程安全的吗
+31. Spring框架中的单例Bean是线程安全的么
+32. spring框架中使用了哪些设计模式及应用场景
+33. Spring容器启动流程是怎样的
+34. Spring如何处理事务
+35. Spring如何处理循环依赖问题
+36. spring事务的实现方式原理以及隔离级别
+37. spring事务什么时候会失效
+38. Spring用到了哪些设计模式mp4
+39. spring支持的bean作用域
+40. Spring中Bean是线程安全的吗mp4
+41. Spring中的Bean创建的生命周期有哪些步骤
+42. Spring中的事务是如何实现的
+43. Spring中什么时候@Transactional会失效
+44. Spring中后置处理器的作用
+45. 如何实现AOP，项目哪些地方用到了AOP
+46. 说说常用的SpringBoot注解，及其实现
+47. 还读过哪些框架源码介绍一下你还熟悉的
+48. 介绍一下Spring，读过源码介绍一下大致流程
+49. 什么时候@Transactional失效
+50. 说一下Spring的事务机制
+51. 对Aop的理解
+52. 对IOC的理解
+53. 简述spring bean的生命周期
+54. Spring用到了哪些设计模式
+55. Spring中Bean是线程安全的吗
+56. 如何理解springboot的starter
+57. 如何实现一个IOC容器
+58. 什么的是bean的自动装配，有哪些方式
--- spring-tiny-knowledge-summary/src/main/java/com/example/anytest/aop_test/Test.java
+++ spring-tiny-knowledge-summary/src/main/java/com/example/anytest/aop_test/Test.java
@@ -1,22 +1,31 @@
 package com.example.anytest.aop_test;
 
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 /**
  * @author gongchengship@163.com
  */
 @Component
-public class Test implements CommandLineRunner {
+public class Test implements CommandLineRunner, ApplicationContextAware {
 
     @Autowired
     private BusinessClassOne businessClassOne;
 
-    @Autowired
+
     ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
     @Override
     public void run(String... args) throws Exception {
         System.out.println(AopUtils.isAopProxy(businessClassOne));
@@ -25,5 +34,14 @@ public class Test implements CommandLineRunner {
         int mun = businessClassOne.getBusinessClassTwo().getBusinessClassThree().getMun();
         System.out.println(mun);
         System.out.println(businessClassOne.doBusinessLogic2(3));
+
+        // 证明容器中 存在DefaultAdvisorAutoProxyCreator 这种类型的 bean
+        try {
+            System.out.println(applicationContext.getBean(DefaultAdvisorAutoProxyCreator.class));
+        } catch (Exception e) {
+            // ignore
+        }
+        System.out.println(applicationContext.getBean(ApplicationContext.class));
     }
+
 }
